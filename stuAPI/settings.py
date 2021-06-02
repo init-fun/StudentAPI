7db50914feb2f5176f0ac2c2a5861e113b4492d4
init_fun <sde.kapilraj@gmail.com>
2021-06-02 18:01:25 +0530

    Update settings.py

--- stuAPI/settings.py
+++ stuAPI/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-fx6%1@wl0lj07nw0tdo^6r9q)@k%5hd2hy-vjjtr6yv916m1&7"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
